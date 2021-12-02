862c2150a45a8d1f5dbe23933c924a1f9cd8ceef
kartalyusuf <yusufkartall97@hotmail.com>
2021-12-02 14:21:38 +0300

    Add login xml file and some xml ui design
    Co-Authored-By: kartalyusuf <51126266+kartalyusuf@users.noreply.github.com>

---
+++ app/src/main/java/com/kartal/bookshop/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.kartal.bookshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
